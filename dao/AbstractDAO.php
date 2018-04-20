100aa67413d2318a50ee7c536378ae61bb3af410
C Server <c.gdd.ita@gmail.com>
2018-04-20 17:36:35 -0500

    (UPDATE) Depurando archivos obsoletos

---
+++ dao/AbstractDAO.php
@@ -0,0 +1,19 @@
+<?php
+date_default_timezone_set("America/Mexico_City");
+
+$conexion = mysql_connect('localhost', 'root', 'Get7usa7') or die ('MySQL Not found // Could Not Connect.');
+
+mysql_select_db('gdd2', $conexion);
+
+function fechaNormal($fecha){
+		$nfecha = date('d/m/Y',strtotime($fecha));
+		return $nfecha;
+}
+
+function calculaFecha($fecha,$dias) {
+    if($fecha=="") {
+        $fecha= date('Y-m-d');
+
+     }
+    return date('Y-m-d', strtotime('+'.$dias.' day', strtotime($fecha)));
+}
