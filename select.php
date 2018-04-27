9aa413f79ef5d329677fe07e96a629f126b52dde
Rigss <Rigss@DESKTOP-FG6V5QH>
2018-04-27 02:31:41 -0500

    Se reemplazaron todos los metodos antiguos que utilizaban mysql_query obsoleto por querySelect en el archivo select.php

--- select.php
+++ select.php
@@ -22,9 +22,12 @@
   <?php } ?>
 
 
+      
+      
+      
      <?php function fillOptionDouble($table, $col) {
           $array = querySelect("select * from ". $table );
-          foreach ($array as $value) {
+          foreach ($array as $fila) {
               ?> <option value="<?php echo $fila[$col]." ".$fila[$col+1]." ".$fila[$col+2]; ?>">
           <?php echo $fila[$col]." ".$fila[$col+1]." ".$fila[$col+2]; ?>
         </option>
@@ -43,19 +46,20 @@
 
 
         
+        
+        
 <?php function OpCarrera($table, $idjefe, $col=0) {
     $listaCarreras = querySelect("select NombreProfesor, ApellidoPaternoProfesor,"
         . " ApellidoMaternoProfesor from '".$table."' inner join "
         . "JefeDepartamento on IdJefeInmediatoProfesor = IdJefeDepartamento"
         . " where idJefeDepartamento = '".$idjefe."'");
-    foreach ($listaCarreras as $value) { ?>
+    foreach ($listaCarreras as $fila) { ?>
          <option value="<?php echo $fila[$col]." ".$fila[$col+1]." ".$fila[$col+2]; ?>">
             <?php echo $fila[$col]." ".$fila[$col+1]." ".$fila[$col+2]; ?>
         </option>
 <?php }}?> 
         
         
-    
     <?php function OPCarrera_($table, $idjefe, $col=0) {
       $lista = mysql_query("select NombreProfesor, ApellidoPaternoProfesor, ApellidoMaternoProfesor from '".$table."' inner join
       JefeDepartamento on IdJefeInmediatoProfesor = IdJefeDepartamento where idJefeDepartamento = '".$idjefe."'");
@@ -68,10 +72,16 @@
       <?php } ?>
 
 
-
+          
+          
+<?php function fillOptionId($table, $k, $v){
+    $listaId = querySelect("select * from ". $table);
+    foreach ($listaId as $fila) { ?>
+         <option value="<?php echo $fila[$k]; ?>"><?php echo $fila[$v]; ?></option>
+<?php}}?>
 
       <!-- llena opcion de acuerdo a una columna y su valor  -->
-      <?php function fillOptionId($table,$k,$v) {
+      <?php function fillOptionId_($table,$k,$v) {
         $lista = mysql_query("select * from ". $table );
         if(mysql_num_rows($lista)>0){
           while($fila = mysql_fetch_array($lista)){ ?>
@@ -80,7 +90,15 @@
         <?php } ?>
 
 
-        <?php function fillOptionsSingle($table,$col) {
+            
+ <?phpfunction fillOptionSingle($table, $col) {
+     $listaSingle = querySelect("select * from ". $table);
+     foreach ($listaSingle as $fila) { ?>
+        <option value="<?php echo $fila[$col]; ?>"><?php echo $fila[$col]; ?></option>
+<?php }}?>          
+            
+        
+        <?php function fillOptionsSingle_($table,$col) {
           $lista = mysql_query("select * from ". $table );
           if(mysql_num_rows($lista)>0){
             while($fila = mysql_fetch_array($lista)){ ?>
@@ -91,9 +109,18 @@
 
 
 
-
+              
+              
+<?phpfunction ismenu() {
+    $listaMenu = querySelect("select role from members "
+        . "WHERE email='".$_SESSION['username']."'" );
+        foreach ($listaMenu as $fila) {
+          $role = $fila['role'];  
+        } return $role;
+}?>
+           
           <?php
-          function ismenu() {
+          function ismenu_() {
             $lista = mysql_query("select role from members WHERE email='".$_SESSION['username']."'" );
             if(mysql_num_rows($lista)>0){
               while($fila = mysql_fetch_array($lista)){
@@ -105,8 +132,17 @@
           ?>
 
 
+              
+<?php function OptieneNombre($correo, $col = 0) {
+   $listaNombre = querySelect("select NombreProfesor, ApellidoPaternoProfesor, "
+            . "ApellidoMaternoProfesor from profesor where"
+            . " CorreoProfesor = '".$correo."' ");
+    foreach ($listaNombre as $fila) { 
+         return $fila[$col] . " " . $fila[$col+1] . " " . $fila[$col+2];
+ }}?>
 
-            <?php function OptieneNombre($correo, $col = 0) {
+              
+            <?php function OptieneNombre_($correo, $col = 0) {
               $lista = mysql_query("select NombreProfesor, ApellidoPaternoProfesor, ApellidoMaternoProfesor from profesor where CorreoProfesor = '".$correo."' ");
               if(mysql_num_rows($lista)>0){
                 while($fila = mysql_fetch_array($lista)){
@@ -116,7 +152,17 @@
             } ?>
 
 
-            <?php function OptieneNombreJefe($correo, $col = 0) {
+              
+<?php function OptieneNombreJefe($correo, $col = 0) {
+    $listaNombreJefe = querySelect("select NombreJefeDepto, ApellidoPaternoJefeDepto,"
+            . " ApellidoMaternoJefeDepto from jefedepartamento where"
+            . " CorreoJefe = '".$correo."' ");
+    foreach ($listaNombreJefe as $fila){
+        return $fila[$col] . " " . $fila[$col+1] . " " . $fila[$col+2];
+}}?>
+              
+              
+            <?php function OptieneNombreJefe_($correo, $col = 0) {
               $lista = mysql_query("select NombreJefeDepto, ApellidoPaternoJefeDepto, ApellidoMaternoJefeDepto from jefedepartamento where CorreoJefe = '".$correo."' ");
               if(mysql_num_rows($lista)>0){
                 while($fila = mysql_fetch_array($lista)){
@@ -126,8 +172,15 @@
             } ?>
 
 
+              
+<?php  function OptieneCorreoBoss($table, $col=0) {
+    $listaCorreoBoss = querySelect("select email from ". $table);
+    foreach ($listaCorreoBoss as $fila) { ?>
+        <option value="<?php echo $fila[$col]; ?>"><?php echo $fila[$col]; ?></option>
+<?php}}?>
 
-            <?php function OptieneCorreoBoss($table,$col = 0) {
+        
+            <?php function OptieneCorreoBoss_($table,$col = 0) {
               $lista = mysql_query("select email from ". $table );
               if(mysql_num_rows($lista)>0){
                 while($fila = mysql_fetch_array($lista)){ ?>
@@ -138,7 +191,18 @@
 
 
 
-            <?php function OptienePresidente($correo, $col = 0) {
+                  
+<?phpfunction OptienePresidente($correo, $col=0) {
+    $listaPresidente = querySelect("select NombrePresidenteAcad, ApellidoPaternoPresidenteAcad,"
+        . " ApellidoMaternoPresidenteAcad from presidenteacademia inner join"
+        . " jefedepartamento on IdDepartamentoPresidente = IdDepartamentoJefe"
+        . " where CorreoJefe ='".$correo."' ");
+        foreach ($listaPresidente as $fila) { 
+             return $fila[$col] . " " . $fila[$col+1] . " " . $fila[$col+2];
+}} ?>
+                  
+                  
+            <?php function OptienePresidente_($correo, $col = 0) {
               $lista = mysql_query("select NombrePresidenteAcad, ApellidoPaternoPresidenteAcad, ApellidoMaternoPresidenteAcad from presidenteacademia inner join jefedepartamento on IdDepartamentoPresidente = IdDepartamentoJefe where CorreoJefe ='".$correo."' ");
               if(mysql_num_rows($lista)>0){
                 while($fila = mysql_fetch_array($lista)){
@@ -148,8 +212,17 @@
             } ?>
 
 
+                  
+<?php function OptieneCarrera($correoJefe, $col=0) {
+     $listaCarrera = querySelect(" select NombreCarrera from carrera inner join"
+            . " jefedepartamento on IdDepartamentoCarrera = IdDepartamentoJefe"
+            . " where CorreoJefe = '".$correoJefe."' ");
+    foreach ($listaCarrera as $fila) {
+         return $fila[$col];
+}} ?>
 
-            <?php function OptieneCarrera($correoJefe,$col = 0) {
+                  
+            <?php function OptieneCarrera_($correoJefe,$col = 0) {
               $lista = mysql_query(" select NombreCarrera from carrera inner join jefedepartamento on IdDepartamentoCarrera = IdDepartamentoJefe where CorreoJefe = '".$correoJefe."' ");
               if(mysql_num_rows($lista)>0){
                 while($fila = mysql_fetch_array($lista)){
@@ -161,7 +234,16 @@
 
 
 
-            <?php function OptieneDepartamento($correoJefe,$col = 0) {
+                  
+<?php function OptieneDepartamento($correoJefe, $col=0) {
+     $listaDepartamento = querySelect(" select NombreDepartamento from departamento"
+            . " inner join jefedepartamento on IdDepartamento = IdDepartamentoJefe"
+            . " where CorreoJefe = '".$correoJefe."' ");
+    foreach ($listaDepartamento as $fila) {
+      return $fila[$col];  
+}} ?>
+                  
+            <?php function OptieneDepartamento_($correoJefe,$col = 0) {
 
               $lista = mysql_query(" select NombreDepartamento from departamento inner join jefedepartamento on IdDepartamento = IdDepartamentoJefe where CorreoJefe = '".$correoJefe."' ");
               if(mysql_num_rows($lista)>0){
@@ -175,7 +257,17 @@
 
 
 
-          <?php function fillpublic($table,$k,$v) {
+                  
+<?php function fullpublic($table,$k,$v) {
+     $lista = querySelect("select * from ". $table. " where sign1=1 and sign2=1");
+     foreach ($lista as $fila) { ?>
+        <option value="<?php echo $fila[$k]; ?>">
+            <?php echo $fila[$v]; ?>
+        </option> 
+<?php }} ?>
+                 
+        
+          <?php function fillpublic_($table,$k,$v) {
             $lista = mysql_query("select * from ". $table. " where sign1=1 and sign2=1");
             if(mysql_num_rows($lista)>0){
               while($fila = mysql_fetch_array($lista)){ ?>
@@ -186,6 +278,16 @@
 
 
 
+                
+                
+<?php function getById($table,$k,$v,$NombreCurso) {
+    $lista = querySelect("SELECT *FROM  " .$table);
+    foreach ($lista as $fila) { ?>
+        <option value="<?php echo $fila[$k]; ?>">
+            <?php echo $fila[$v]; ?>
+        </option>
+<?php }} ?>
+                
               <?php function getById_($table,$k,$v,$NombreCurso) {
                 $SQL = "SELECT *FROM  " .$table;
 
