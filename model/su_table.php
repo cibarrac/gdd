705c3819c0b97c6bb282d70b398129c474b4ebf5
Christian Ibarra Cruz <cibarra@innsytech.com>
2018-04-29 13:41:59 -0500

    Tabla su_tabla mysqli new

--- model/su_table.php
+++ model/su_table.php
@@ -39,114 +39,41 @@
          echo "<th class='header'>". $field[0] ."</th>";
      }    
  }
- function getheaders_($table)
- {        $out = array();
-        $campos = mysql_query("describe ".$table);
-
-                    if(mysql_num_rows($campos)>0){
-                        while($r = mysql_fetch_array($campos)){
-                        $out [] = $r[0];
-                        }}
-
-        $header = $out;
-        foreach ($header as $value) { ?>
-                        <th class="header"> <?php echo  $value;?></th>
-<?php }?>
-
-
-
-<?php
- }
+ 
  function createTable($view){
          include 'listViews.php';
-         $table = $table[$view];
-
-        // include "Buscar.php";
-         ?>
-
-
+         $table = $table[$view];  ?>
          <div class="col-md-3">
-
               <?php
-
                 $modal = new Modal('btn1',$view,$view,"Insertar ".$view);
                 $modal->getContent();
-
                 $_subscribe = new Modal('inscripcion_modal', 'Inscripción al curso', '_Inscripciones', 'Inscribir');
                 $_subscribe->getContent(true);
-
               ?>
-        </div>
-
-
-        <br> <br>
+        </div> <br> <br>
         <table class="table table-hover table-striped table-responsive" id="tabla">
-     <thead>
-        <tr>
-          <?php getheaders($table);
-          ?>
-
-        </tr>
-     </thead>
+     <thead> <tr> <?php getheaders($table); ?> </tr>  </thead>
      <tbody>
-
-
-    <?php  $list = mysql_query("SELECT  * FROM ".$table);
-    $i = 0;
-    if(mysql_num_rows($list)>0){
-             while($row = mysql_fetch_array($list)){   ?>
-                   <tr <?php
-                   if(isset($row['ispublic'])){
-                        if($row['ispublic']==1){ echo "class='success'";} else {echo "class='warning'";}
-                   }
-
-
-                   ?> >
-                   <?php
-                   $flag = true;
+    <?php  $list = querySelect("SELECT  * FROM ".$table);
+              $i = 0;
+             foreach($list as $row) {   ?>       
+                   <tr <?php if(isset($row['ispublic'])){
+                                 if($row['ispublic']==1){ echo "class='success'";} 
+                                 else {echo "class='warning'";}
+                           } ?> >
+                   <?php $flag = true;
                    foreach ($row as $col) {
-                                 //print_r($row);
                                 if($flag) { ?>
-
-                                        <td>
-                                        <?php
-                                           echo $col; ?>
-                                        </td>
-                                 <?php $flag = false; } else { $flag=true;}
-
-                                }  ?>
-                                <td>
-                                    <?php
-                                          evaluate_cursos($table,$row['NumeroCurso']);
-                                          //$modal = new Modal('btn1','Tabla',$contentView,'?');
-                                          //$modal->getContent();
-                                          ?>
-
-                                        </td>
-                </tr>
-                <?php $i++; }  } else {
-
-                        ?>
-                                <tr> <td colspan="6">No se encontraron resultados <?php echo $table; ?> </td> </tr>
-                        <?php  } ?>
-
-
+                                <td> <?php echo $col; ?> </td>
+                                 <?php $flag = false; } else { $flag=true;} }  ?>
+                                <td> <?php evaluate_cursos($table,$row['NumeroCurso']); ?> </td>
+                    </tr> <?php $i++; }   
+                     ?>
             </tbody>
     </table>
-
 <?php  } ?>
-
-
 <!-- Fin Tabla Curso ****************************************************************************************************************-->
-
-
-
 <!-- Inicio Tabla Inscripcion ****************************************************************************************************************-->
-
-
-
-
-
 <script>
 
   function inscribir(numerocurso) {
