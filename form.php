39fb5902147742eaa55998fff9a3dd2e151e3f59
Christian Ibarra Cruz <cibarra@innsytech.com>
2018-04-28 21:43:43 -0500

    cleaning form and indent

--- form.php
+++ form.php
@@ -1,35 +1,24 @@
-
-<?php
-  include 'listViews.php';
+<?php include 'listViews.php';
 /**
  * En este metodo se debe poner la tabla y la vista a la que pertenece
  * esta funcion construye un formulario para registro ya incluyendole los parametros necesarios para el post
  */
-
 function createView($view) {
-  include 'listViews.php';
+   include 'listViews.php';
   ?>
-
 <form class="form" action="post.php" method="post">
-
           <!-- Parametros start -->
           <input type="hidden" name="view"  value="<?php echo $view;?>">
           <input type="hidden"  name="table"  value="<?php echo $table[$view];?>">
           <input id="action" type="hidden"  name="action"  value="save">
           <!-- Parametros end -->
-
-            <?php
-         include "views/".$view."View.php";  ?>
+            <?php include "views/".$view."View.php";  ?>
 
         <div class="container">
-
           <button type="submit"  id="submit" data-original-title="Contactos"
           class="btn btn-success btn-fab btn-float" onclick="setAction('save')"> <i class="fa fa-save fa-2x"></i></button>
          <!--  <button type="submit" class="btn btn-warning" onclick="setAction('update')"> Editar</button>
          -->
         </div>
 </form>
-
-
-
 <?php } ?>
