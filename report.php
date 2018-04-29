d3d42d3deb7089079ab85ba8a5e81804decd4631
C Server <c.gdd.ita@gmail.com>
2018-04-29 14:20:58 -0500

    Se coloco titulo y se indento el archivo report.php

--- report.php
+++ report.php
@@ -1,4 +1,6 @@
-<?php include "conn.php";
+<?php 
+/* Archivo para rerealizar e imprimir las listas de asistencia de los profesores a los cursos
+include "conn.php"; */
       include "model/enty.php";
       include 'bs/QueryService.php';
       include 'bs/ReportService.php'; ?>
@@ -15,63 +17,69 @@
     <script src="assets/js/bootstrap.js"></script>
 </head>
 <body>
-     <div class="container">
-     <br><br>
-     <div class="row">
+    
+<div class="container">
+    <br><br>
+    <div class="row">
         <div class="col-md-12">
-             <label for="">Nombre del curso  <u> <?php  echo $nombre;?> </u> </label>
-             <label for="">Lugar  <u><?php echo $aula;?> </u></label>
-             <label for="">Instructor (s)  <u><?php echo $idinstructor;?> </u> </label>
-             <label for="">Periodo  <u><?php echo $periodo;?></u></label>
-             <label for="">Fecha <u> <?php echo $fechaalta;?></u> </label>
-             <label for="">Horario  <u><?php echo $horainicio;?> </u></label>
+            <label for="">Nombre del curso  <u> <?php  echo $nombre;?> </u> </label>
+            <label for="">Lugar  <u><?php echo $aula;?> </u></label>
+            <label for="">Instructor (s)  <u><?php echo $idinstructor;?> </u> </label>
+            <label for="">Periodo  <u><?php echo $periodo;?></u></label>
+            <label for="">Fecha <u> <?php echo $fechaalta;?></u> </label>
+            <label for="">Horario  <u><?php echo $horainicio;?> </u></label>
         </div>
- </div>
- <br>
-  <div class="row">
-            <div class="col-md-12">
-                <table class="table table-bordered">
-                    <thead style="background:#dad9d9">
-                        <tr>
-                            <th>No.</th>
-                            <th>Grado Academico</th>
-                            <th>Docente</th>
-                            <th>Area</th>
-                            <th>No. Tarjeta</th>
-                            <th>RFC</th>
-                            <th>Firma:</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                       <?php  $list = ReportService::getList();
-                       $i=1;
-                        foreach ($list as $profe) { ?>
-                                 <tr>
-                                   <td> <?php echo $i;?> </td>
-                                   <td> <?php echo $profe->__GET('gradoestudios');?> </td>
-                                   <td> <?php echo $profe->__GET('nombre');?> </td>
-                                   <td> <?php echo $profe->__GET('departamento');?> </td>
-                                   <td> <?php echo $profe->__GET('numerotarjeta');?> </td>
-                                   <td> <?php echo $profe->__GET('rfc');?> </td>
-                                   <td> <?php echo ""; ?> </td>
+    </div>
+    <br>
+ 
+    <div class="row">
+        <div class="col-md-12">
+            <table class="table table-bordered">
+                <thead style="background:#dad9d9">
+                    <tr>
+                        <th>No.</th>
+                        <th>Grado Academico</th>
+                        <th>Docente</th>
+                        <th>Area</th>
+                        <th>No. Tarjeta</th>
+                        <th>RFC</th>
+                        <th>Firma:</th>
+                    </tr>
+                </thead>
 
-                                 </tr>
-                             <?php $i++;}
-                           ?>
-                    </tbody>
-                </table>
-            </div>
-            </div>
-      <div class="row">
-           <div class="col-md-6">
-               <label for="">MC. DANIEL MULATO AGUERO</label>
-           </div>
-           <div class="col-md-6">
-              <label for="">  MC. NOE CASTELLANOS BEBOLLEDO  </label></div>
-         
-      </div>
+                <tbody>
+                   <?php  $list = ReportService::getList();
+                   $i=1;
+                    foreach ($list as $profe) { ?>
+                             <tr>
+                               <td> <?php echo $i;?> </td>
+                               <td> <?php echo $profe->__GET('gradoestudios');?> </td>
+                               <td> <?php echo $profe->__GET('nombre');?> </td>
+                               <td> <?php echo $profe->__GET('departamento');?> </td>
+                               <td> <?php echo $profe->__GET('numerotarjeta');?> </td>
+                               <td> <?php echo $profe->__GET('rfc');?> </td>
+                               <td> <?php echo ""; ?> </td>
 
-      </div>
+                             </tr>
+                         <?php $i++;}
+                       ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <div class="row">
+         <div class="col-md-6">
+             <label for="">MC. DANIEL MULATO AGUERO</label>
+         </div>
+
+         <div class="col-md-6">
+            <label for="">  MC. NOE CASTELLANOS BEBOLLEDO  </label>
+         </div>
+
+    </div>
+
+</div>
 </body>
 </html>
 
