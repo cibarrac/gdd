f913cd787532a4d5a1d314d1ad441118d4c7b3cf
C Server <c.gdd.ita@gmail.com>
2018-05-03 18:44:11 -0500

    Se agrego la consutla que obtiene los profesores inscritos a un curso para el reporte de asistencia en SQL.php

--- bs/SQL.php
+++ bs/SQL.php
@@ -1,8 +1,21 @@
 <?php
 class SQL {
     
-    public static $CURSOS_ACTIVOS = "SELECT NumeroCurso, NombreCurso, ObjetivoCurso,"
-                . " FechaInicioCurso, FechaFinCurso, TipoCurso,"
-                . " PeriodoCurso from curso";
+    public static $CURSOS_OFERTADOS = "SELECT NumeroCurso, NombreCurso, ObjetivoCurso,"
+            . " FechaInicioCurso, FechaFinCurso, TipoCurso,"
+            . " PeriodoCurso from curso";
+    
+    
+    public static $LISTA_ASISTENCIA = "SELECT INSCRIPCION.NombreCurso, "
+            . "PROFESOR.NombreProfesor, PROFESOR.ApellidoPaternoProfesor, "
+            . "PROFESOR.ApellidoMaternoProfesor, PROFESOR.RFCProfesor, "
+            . "PROFESOR.NumeroTarjetaProfesor, PROFESOR.GradoEstudiosProfesor, "
+            . "INSCRIPCION.NumeroCurso, CARRERA.NombreCarrera from profesor "
+            . "PROFESOR, inscripcion INSCRIPCION, carrera CARRERA WHERE "
+            . "PROFESOR.IdProfesor = INSCRIPCION.IdProfesor AND "
+            . "INSCRIPCION.NumeroCurso = '5.1' AND "
+            . "CARRERA.IdCarrera = PROFESOR.IdCarrera"; 
+
+     
     
 }
