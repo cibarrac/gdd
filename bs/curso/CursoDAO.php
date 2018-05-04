d77d1add597c827a3c79f3959c9b7bf16bea9fff
C Server <c.gdd.ita@gmail.com>
2018-05-03 22:07:05 -0500

    Se agrego la funcion cargarAsistencia en CursoDAO.php

--- bs/curso/CursoDAO.php
+++ bs/curso/CursoDAO.php
@@ -8,12 +8,13 @@ class CursoDAO {
     
     public function cargarCursos() {
        
-        $result= querySelect(SQL::$CURSOS_ACTIVOS);
+        $result= querySelect(SQL::$CURSOS_OFERTADOS);
         $cursoList = array();
         foreach($result as $row)
         {
             $curso = new Curso();
             $curso->__SET("NumeroCurso", $row['NumeroCurso']); 
+            $curso->__SET("NombreCurso", $row['NombreCurso']);
             $curso->__SET("ObjetivoCurso", $row['ObjetivoCurso']);
             $curso->__SET("FechaInicioCurso", $row['FechaInicioCurso']);
             $curso->__SET("FechaFinCurso", $row['FechaFinCurso']);
@@ -23,7 +24,25 @@ class CursoDAO {
         }
         return $cursoList;
     }
+    
+    
+    
+    
     public function cargarAsistencias() {
-        return array();
+        $result= querySelect(SQL::$LISTA_ASISTENCIA);
+        $cursoList = array();
+        foreach($result as $row)
+        {
+            $curso = new Curso();
+            $curso->__SET("NumeroCurso", $row['NumeroCurso']); 
+            $curso->__SET("NumeroTarjeta", $row['NumeroTarjetaProfesor']);
+            $curso->__SET("Grado", $row['GradoEstudiosProfesor']);
+            $curso->__SET("Participante", $row['NombreProfesor']." ".$row['ApellidoPaternoProfesor']." ".$row['ApellidoMaternoProfesor']);
+            $curso->__SET("Carrera", $row['NombreCarrera']);
+            $curso->__SET("RFC", $row['RFCProfesor']);
+            $curso->__SET("Firma", $row[' ']);
+            $cursoList [] = $curso;
+        }
+        return $cursoList;
     }
 }
