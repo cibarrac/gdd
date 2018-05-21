6f948429cfc90e357700a1ae8e8f00c4988a5a09
C Server <c.gdd.ita@gmail.com>
2018-05-21 12:38:52 -0500

    Quitando la referencia a jquery en el index. Se recupero el archivo conn.php

--- index.php
+++ index.php
@@ -21,14 +21,15 @@ if(isset($_GET['view'])){  $contentView = $_GET['view']; } ?>
         <meta content='width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;' name='viewport' />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="assets/css/bootstrap.css">
-        <script src="assets/js/jquery-1.11.3.min.js"></script>
+         <script src="https://code.jquery.com/jquery-1.12.4.js" charset="utf-8"></script>
+       
+       <!-- <script src="assets/js/jquery-1.11.3.min.js"></script> -->
         <script src="assets/js/bootstrap.js"></script>
         <script type="text/javascript" src="assets/main.js"></script>
         
         
        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
         <link rel="stylesheet" href="assets/css/buttons.dataTables.css">
-        <script src="https://code.jquery.com/jquery-1.12.4.js" charset="utf-8"></script>
         <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js" charset="utf-8"></script>
         <script src="https://cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.min.js" charset="utf-8"></script>
         <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.colVis.min.js" charset="utf-8"></script>
