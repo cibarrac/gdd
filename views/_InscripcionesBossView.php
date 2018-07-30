00eab9078a8a6a8b56e5d95ccc714a903b897d04
C Server <c.gdd.ita@gmail.com>
2018-07-30 03:44:59 -0500

    Se corrigio _InscripcionesBossView.php no guardaba en la base de datos

--- views/_InscripcionesBossView.php
+++ views/_InscripcionesBossView.php
@@ -8,7 +8,7 @@
     <div class="form-group">
       <label for="">Nombre del curso: </label>
       <select class="form-control" name="NombreCurso" id="NombreCurso" onchange="getCurso()" required>
-        <option value=""></option>
+        <option value="Seleccionar"> Seleccionar</option>
         <?php
         $numero = fillOptionsSingle("curso",1)
         ?>
@@ -19,7 +19,7 @@
   <div class="col-md-3">
     <div class="form-group">
       <label for="">Numero del curso: </label>
-      <input type="text" class="form-control" name="NumeroCurso" id="NumeroCurso" value=" ">
+      <input type="text" class="form-control" name="NumeroCurso" id="NumeroCurso" value="">
     </div>
   </div>
 
@@ -30,8 +30,7 @@
 
 
 <div class="row">
-
-
+    
   <div class="col-md-3">
     <div class="form-group">
       <label for="">Fecha de inicio:</label>
@@ -45,7 +44,7 @@
       <input type="date" name="FechaFin" id="FechaFin" class="form-control"readonly=”readonly” >
     </div>
   </div>
-
+    
 </div>
 
 
@@ -55,15 +54,8 @@
 
   <div class="col-md-6">
     <div class="form-group">
-      <label for="">Instructor 1:</label>
-      <input type="text" class="form-control" name="NombreInstructor1"  id="NombreInstructor1" readonly=”readonly” >
-    </div>
-  </div>
-
-  <div class="col-md-6">
-    <div class="form-group">
-      <label for="">Instructor 2 (En caso de ser necesario):</label>
-      <input type="text" class="form-control" name="NombreInstructor2"  id="NombreInstructor2"readonly=”readonly” >
+      <label for="">Nombre de instructor:</label>
+      <input type="text" class="form-control" name="NombreInstructor"  id="NombreInstructor" readonly=”readonly” >
     </div>
   </div>
 
@@ -89,20 +81,21 @@
     </div>
   </div>
 </div>
-<div class="row">
-  <div class="col-md-6">
-    <div class="from-group">
-      <label for="">Inscribir al profesor:  </label>
-      <!-- CONSULTA: Aqui solo tiene que mostrar profesores solo de su carrera. -->
 
-      <select class="form-control"  name="IdProfesor" >
-        <option value=""> </option>
-        <?php
-           ProfesoresPorDepartamento($_SESSION['username'],1)
-        ?>
-      </select>
+
+<div class="row">
+<div class="col-md-6">
+        <div class="from-group">
+            <label for="">Inscribir al profesor:  </label>
+            <select class="form-control"  name="NombreProfesorInscrito" id="NombreProfesorInscrito" onchange="getId()">
+                <option value=""> </option>
+                   <?php fillNombreCompletoProfesor() ?>
+          </select>
+        </div>
     </div>
-  </div>
+    
+    <input type="text" name="IdProfesor" id="IdProfesor" hidden>
+    
 </div>
 
 
@@ -122,22 +115,24 @@ function getCurso() {
       var response = this.response;
 
       if (typeof response === 'undefined' || response === "")
-      throw "No se recuperó la información de la respuesta a la petición."
+             throw "No se recuperó la información de la respuesta a la petición.";
 
       var res = JSON.parse(response);
-
-      if (res.status == 200) {
+ console.log(res);
+     
+       if (res.status == 200) {
         var data = res.data;
 
         if (data.length == 0)
         return;
-
+       
+       
         // Las propiedades del objeto 'data' deben ser igual al nombre del campo SQL
         document.querySelector('#NumeroCurso').value = data.NumeroCurso;
         document.querySelector('#FechaInicio').value = data.FechaInicioCurso;
         document.querySelector('#FechaFin').value = data.FechaFinCurso;
-        document.querySelector('#NombreInstructor1').value = data.NombreCompletoInstructor1;
-        document.querySelector('#NombreInstructor2').value = data.NombreCompletoInstructor2;
+        document.querySelector('#NombreInstructor').value = data.NombreCompletoInstructor;
+        //(document.querySelector('#NombreInstructor2').value = data.NombreCompletoInstructor2;
         document.querySelector('#TipoCurso').value = data.TipoCurso;
         //document.querySelector('#horario').value = data.horainicio;
         document.querySelector('#Aula').value = data.AulaPropuesta;
