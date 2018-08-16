b7a47f9a6c0faddd27a82b4bf0b3b16606775c86
C Server <c.gdd.ita@gmail.com>
2018-08-15 20:45:54 -0500

    Se agrego boton editar curso, se agrego la referencia a la api

--- model/menubar/su_table.php
+++ model/menubar/su_table.php
@@ -6,11 +6,14 @@
      <button class="btn btn-info fa fa-print" onclick="reportBy(<?php echo $NumeroCurso;?>);">
      </button>
 
-     <button  type="checkbox" class="btn btn-warning fa fa-edit" onclick="firmar(<?php echo $NumeroCurso;?>,<?php echo $NumeroCurso;?>);">
+     <button  type="checkbox" class="btn btn-warning fa fa-check-square-o" onclick="firmar(<?php echo $NumeroCurso;?>,<?php echo $NumeroCurso;?>);">
      </button>
 
-   <button  type="checkbox" class="btn btn-default fa fa-drivers-license"  data-toggle="modal" data-target="#inscripcion_modal" onclick="inscribir('<?php echo $NumeroCurso;?>');">
+    <button  type="checkbox" class="btn btn-default fa fa-drivers-license"  data-toggle="modal" data-target="#inscripcion_modal" onclick="inscribir('<?php echo $NumeroCurso;?>');">
      </button>
+
+    <button type="checkbox" class="btn btn-success fa fa-pencil" data-toggle="modal" data-target="#editar_modal" onclick="infoCurso('<?php echo $NumeroCurso;?>');" >  
+    </button>
 
 <?php
 /*
@@ -43,12 +46,18 @@
         $table = $table[$view];  ?>
         <div class="col-md-3">
               <?php
-                $modal = new Modal('btn1',$view,$view,"Insertar ".$view);
-                $modal->getContent();
+                $modalInsert = new Modal('btn1',$view,$view,"Insertar ".$view);
+                $modalInsert->getContent();
                 $_subscribe = new Modal('inscripcion_modal', 'Inscripción al curso', '_Inscripciones', 'Inscribir');
                 $_subscribe->getContent(true);
+                
+                $_edit = new Modal('editar_modal', 'Editar', '_Curso', 'Editar');
+                $_edit->getContent(true);
               ?>
-        </div> <br> <br>
+        </div>
+        
+
+       <br> <br>
     <table class="table table-hover table-striped table-responsive" id="tabla">
         <thead> <tr> <?php getheaders($table); ?> </tr>  </thead>
         <tbody>
@@ -120,6 +129,48 @@
     request.send();
   }
   
+  
+  
+  
+  function infoCurso(numeroCurso) {
+    var request = new XMLHttpRequest();
+    request.onload = function () {
+      var response = this.response;
+
+      if (typeof response === 'undefined' || response === "")
+      throw "No se recuperó la información de la respuesta a la petición."
+
+      var res = JSON.parse(response);
+
+      if (res.status == 200) {
+        var data = res.data;
+
+        if (data.length == 0)
+        return;
+
+     // Las propiedades del objeto 'data' deben ser igual al nombre del campo SQL
+        document.querySelector('#NombreCompletoPresiAcad').value = data.NombreCompletoPresiAcad;
+        document.querySelector('#NombreCompletoJefeDepto').value = data.NombreCompletoJefeDepto;
+        document.querySelector('#IdDepartamentoDe').value = data.NombreDepartamento;
+        document.querySelector('#NombreCurso').value = data.NombreCurso;
+        document.querySelector('#ObjetivoCurso').value = data.ObjetivoCurso;
+        document.querySelector('#NumeroCurso').value = data.NumeroCurso;
+        document.querySelector('#TipoCurso').value = data.TipoCurso;
+        document.querySelector('#NombreCompletoInstructor').value = data.NombreCompletoInstructor;
+        document.querySelector('#AulaPropuesta').value = data.AulaPropuesta;
+        document.querySelector('#Turno').value = data.Turno;
+        document.querySelector('#FechaLimite').value = data.FechaLimite;
+        document.querySelector('#HoraInicioCurso').value = data.HoraInicioCurso;
+        document.querySelector('#HoraFinCurso').value = data.HoraFinCurso;
+        document.querySelector('#FechaInicioCurso').value = data.FechaInicioCurso;
+        document.querySelector('#FechaFinCurso').value = data.FechaFinCurso;
+        document.querySelector('#capacidadmaxima').value = data.capacidadmaxima;
+        document.querySelector('#PeriodoCurso').value = data.PeriodoCurso;
+      }
+    };
+    request.open('GET', 'api.php?oper=getInfoCurso&numeroCurso='  + numeroCurso, true);
+    request.send();
+  }
   
 </script>
 
