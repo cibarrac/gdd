cf14dc125d8158552a0ddb0be270ad9b9828bdaf
C Server <c.gdd.ita@gmail.com>
2018-08-18 01:33:15 -0500

    Se agrego thumbnails en la vista de cursos por la tabla

--- model/menubar/boss_table.php
+++ model/menubar/boss_table.php
@@ -4,9 +4,7 @@
   {
         if($table=="curso") {
       ?>
-     <button class="btn btn-info fa fa-print" onclick="reportBy(<?php echo $NumeroCurso;?>);"> </button>
-     <button  type="checkbox" class="btn btn-default fa fa-drivers-license"  data-toggle="modal" data-target="#inscripcion_modal" onclick="inscribir('<?php echo $NumeroCurso;?>');"></button>
-   
+    
 
 <?php
 
@@ -64,6 +62,43 @@
 
 
         <br> <br>
+        <?php 
+            if($table == "curso")
+            {
+                $idCarreraJefeDepto = getIdCarreraJefe( $_SESSION['username'] );
+                $list = querySelect(SQL::$CURSOS_POR_CARRERA." '%".$idCarreraJefeDepto."%' ");
+                 foreach($list as $row) { ?>
+
+                    <div class="row">
+                        <div class="col-md-12 ">
+                            <div class="thumbnail"  <?php
+                            if($row['ispublic']==1){ echo "style= 'background-color: #b9f6ca;'" ;} 
+                                 else {echo "style= 'background-color: #fff9c4;'";} ?> > 
+                                
+                                <div class="caption">
+                                <h3><?php echo $row['NumeroCurso']." ".$row['NombreCurso'];?></h3>  
+                                <h4>Objetivo:</h4>
+                                <p><?php echo $row['ObjetivoCurso']; ?></p>
+                                <p>Horario: de <?php echo $row['HoraInicioCurso']." a ".$row['HoraFinCurso'];?> <br>  Fecha: del <?php echo $row['FechaInicioCurso']." al ".$row['FechaFinCurso'];?></p>
+                                <h4><p align="right"> Instructor (a): <?php echo $row['NombreCompletoInstructor']; ?> </p></h4>
+                                <button class="btn btn-info fa fa-print" onclick="reportBy(<?php echo $row['NumeroCurso'];?>);"> Imprimir lista
+                                </button>
+                                <button  type="checkbox" class="btn btn-default fa fa-drivers-license"  data-toggle="modal" data-target="#inscripcion_modal" onclick="inscribir('<?php echo $row['NumeroCurso'];?>');"> Inscribir
+                                </button>
+
+                                <button type="checkbox" class="btn btn-success fa fa-pencil" data-toggle="modal" data-target="#editar_modal" onclick="infoCurso('<?php echo $row['NumeroCurso'];?>');" > Editar contenido  
+                                </button>
+                                
+                                
+                                </div>
+                            </div>
+                        </div>
+                    </div> <?php }  }
+                
+            else{
+            
+        ?>
+        
         <table class="table table-hover table-striped table-responsive" id="tabla">
      <thead>
         <tr>
@@ -76,19 +111,15 @@
 
 
     <?php
-    if($table == "curso")
-    {
-      $idCarreraJefeDepto = getIdCarreraJefe( $_SESSION['username'] );
-      $list = querySelect(SQL::$CURSOS_POR_CARRERA." '%".$idCarreraJefeDepto."%' ");
-    }
     
-    elseif($table == "profesor")
+    
+    if($table == "profesor")
     {
         $idJefeDepto = getIdJefe($_SESSION['username']);
         $list = querySelect(SQL::$PROFESORES_POR_JEFE." ".$idJefeDepto);
     }
     
-     elseif($table == "inscripcion")
+     if($table == "inscripcion")
     {
         $idCarreraJefe = getIdCarreraJefe($_SESSION['username']);
         $list = querySelect(SQL::$INSCRIPCIONES_POR_JEFE." ".$idCarreraJefe);
@@ -117,7 +148,7 @@
             </tbody>
     </table>
 
-<?php  } ?>
+ <?php  } }?>
 <script>
 
   function inscribir(numerocurso) {
