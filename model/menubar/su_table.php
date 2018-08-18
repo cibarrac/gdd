c7471f540ff13377e09ac11c00e4a1259a16c20a
C Server <c.gdd.ita@gmail.com>
2018-08-18 00:56:21 -0500

    Se reemplazo la tabla en a vista detectar necesidases por thumbnails

--- model/menubar/su_table.php
+++ model/menubar/su_table.php
@@ -3,25 +3,14 @@
   {
     if($table=="curso") {
       ?>
-     <button class="btn btn-info fa fa-print" onclick="reportBy(<?php echo $NumeroCurso;?>);">
-     </button>
-
-     <button  type="checkbox" class="btn btn-warning fa fa-check-square-o" onclick="firmar(<?php echo $NumeroCurso;?>,<?php echo $NumeroCurso;?>);">
-     </button>
-
-    <button  type="checkbox" class="btn btn-default fa fa-drivers-license"  data-toggle="modal" data-target="#inscripcion_modal" onclick="inscribir('<?php echo $NumeroCurso;?>');">
-     </button>
-
-    <button type="checkbox" class="btn btn-success fa fa-pencil" data-toggle="modal" data-target="#editar_modal" onclick="infoCurso('<?php echo $NumeroCurso;?>');" >  
-    </button>
 
 <?php
 /*
      //En esta parte se pondria el evento click, que asociará el id del curso, obtendrá los datos y los enviara a
      //la vista _InscripcionesView.php
 */
-}
 
+    }
 }
   
 
@@ -58,6 +47,45 @@
         
 
        <br> <br>
+       
+       <?php  if($table == "curso")
+       {
+         $list = querySelect(SQL::$SELECCIONA_TODO." ".$table);
+                foreach($list as $row) { ?>
+
+                    <div class="row">
+                        <div class="col-md-12 ">
+                            <div class="thumbnail"  <?php
+                            if($row['ispublic']==1){ echo "style= 'background-color: #b9f6ca;'" ;} 
+                                 else {echo "style= 'background-color: #fff9c4;'";} ?> > 
+                                
+                                <div class="caption">
+                                <h3><?php echo $row['NumeroCurso']." ".$row['NombreCurso'];?></h3>  
+                                <h4>Objetivo:</h4>
+                                <p><?php echo $row['ObjetivoCurso']; ?></p>
+                                <p>Horario: de <?php echo $row['HoraInicioCurso']." a ".$row['HoraFinCurso'];?> <br>  Fecha: del <?php echo $row['FechaInicioCurso']." al ".$row['FechaFinCurso'];?></p>
+                                <h4><p align="right"> Instructor (a): <?php echo $row['NombreCompletoInstructor']; ?> </p></h4>
+                                <p>Autorizado por desarrollo academico: <?php if($row['sign1']==1){ echo ' Si'; } else { echo ' No'; }?> </p>
+                                <p>Autorizado por subdireccion academica: <?php if($row['sign2']==1){ echo ' Si'; } else { echo ' No'; } ?></p>
+                                
+                                <button  type="checkbox" class="btn btn-warning fa fa-check-square-o" onclick="firmar(<?php echo $row['NumeroCurso'];?>,<?php echo $row['NumeroCurso'];?>);"> Autorizar
+                                </button>
+                                <button class="btn btn-info fa fa-print" onclick="reportBy(<?php echo $row['NumeroCurso'];?>);"> Imprimir lista
+                                </button>
+                                <button  type="checkbox" class="btn btn-default fa fa-drivers-license"  data-toggle="modal" data-target="#inscripcion_modal" onclick="inscribir('<?php echo $row['NumeroCurso'];?>');"> Inscribir
+                                </button>
+
+                                <button type="checkbox" class="btn btn-success fa fa-pencil" data-toggle="modal" data-target="#editar_modal" onclick="infoCurso('<?php echo $row['NumeroCurso'];?>');" > Editar contenido  
+                                </button>
+                                
+                                
+                        </div>
+                </div>
+            </div>
+        </div> <?php }   
+       }
+       else{
+       ?>
     <table class="table table-hover table-striped table-responsive" id="tabla">
         <thead> <tr> <?php getheaders($table); ?> </tr>  </thead>
         <tbody>
@@ -79,7 +107,7 @@
                     </tr> <?php $i++; }  ?>
         </tbody>
     </table>
-<?php  } ?>
+ <?php  }} ?>
 <!-- Fin Tabla Curso ****************************************************************************************************************-->
 <!-- Inicio Tabla Inscripcion ****************************************************************************************************************-->
 <script>
