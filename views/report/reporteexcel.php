5ff49b9b4b97beb60d4cfbb8a68f821621ba0941
C Server <c.gdd.ita@gmail.com>
2018-08-22 00:44:47 -0500

    reporte de exel

---
+++ views/report/reporteexcel.php
@@ -0,0 +1,176 @@
+<?php
+    include_once '../../bs/QueryService.php';
+        
+	$consulta = querySelect("SELECT INSCRIPCION.NombreCurso, PROFESOR.NombreProfesor, PROFESOR.ApellidoPaternoProfesor, PROFESOR.ApellidoMaternoProfesor, PROFESOR.NumeroTarjetaProfesor, PROFESOR.GradoEstudiosProfesor, INSCRIPCION.NumeroCurso, CARRERA.NombreCarrera from profesor PROFESOR, inscripcion INSCRIPCION, carrera CARRERA WHERE PROFESOR.IdProfesor = INSCRIPCION.IdProfesor AND CARRERA.IdCarrera = PROFESOR.IdCarrera AND INSCRIPCION.NumeroCurso = 2.1");
+	if(count($consulta) > 0 ){
+						
+		date_default_timezone_set('America/Mexico_City');
+
+		if (PHP_SAPI == 'cli')
+			die('Este archivo solo se puede ver desde un navegador web');
+
+		/** Se agrega la libreria PHPExcel */
+		require_once '../../assets/lib/PHPExcel/PHPExcel.php';
+
+		// Se crea el objeto PHPExcel
+		$objPHPExcel = new PHPExcel();
+
+		// Se asignan las propiedades del libro
+		$objPHPExcel->getProperties()->setCreator("Codedrinks") //Autor
+							 ->setLastModifiedBy("Codedrinks") //Ultimo usuario que lo modificó
+							 ->setTitle("Reporte Excel con PHP y MySQL")
+							 ->setSubject("Reporte Excel con PHP y MySQL")
+							 ->setDescription("Reporte de alumnos")
+							 ->setKeywords("reporte alumnos carreras")
+							 ->setCategory("Reporte excel");
+
+		$tituloReporte = "Lista de asistencia";
+		$titulosColumnas = array('N.o', 'Numero tarjeta', 'Grado', 'Participante','Departamento/Carrera','Firma');
+		
+		$objPHPExcel->setActiveSheetIndex(0)
+        		    ->mergeCells('A1:F1');
+						
+		// Se agregan los titulos del reporte
+		$objPHPExcel->setActiveSheetIndex(0)
+					->setCellValue('A1',$tituloReporte)
+        		    ->setCellValue('A3',  $titulosColumnas[0])
+		            ->setCellValue('B3',  $titulosColumnas[1])
+        		    ->setCellValue('C3',  $titulosColumnas[2])
+            		->setCellValue('D3',  $titulosColumnas[3])
+                        ->setCellValue('E3',  $titulosColumnas[4])
+                        ->setCellValue('F3',  $titulosColumnas[5]);
+		
+		//Se agregan los datos de los alumnos
+		$i = 4;
+		while ($fila = $consulta->fetch_array()) {
+			$objPHPExcel->setActiveSheetIndex(0)
+        		    ->setCellValue('A'.$i,  $fila[$i])
+		            ->setCellValue('B'.$i,  $fila['NumeroTajetaProfesor'])
+        		    ->setCellValue('C'.$i,  $fila['GradoEstudiosProfesor'])
+                            ->setCellValue('D'.$i,  $fila['NombreProfesor']." ".$fila['ApellidoPaternoProfesor']." ".$fila['ApellidoMaternoProfesor'])
+                            ->setCellValue('E'.$i,  $fila['NombreCarrera'])
+                            ->setCallValue('F'.$i,  $fila['']);
+					$i++;
+		}
+		
+		$estiloTituloReporte = array(
+        	'font' => array(
+	        	'name'      => 'Verdana',
+    	        'bold'      => true,
+        	    'italic'    => false,
+                'strike'    => false,
+               	'size' =>16,
+	            	'color'     => array(
+    	            	'rgb' => 'FFFFFF'
+        	       	)
+            ),
+	        'fill' => array(
+				'type'	=> PHPExcel_Style_Fill::FILL_SOLID,
+				'color'	=> array('argb' => 'FF220835')
+			),
+            'borders' => array(
+               	'allborders' => array(
+                	'style' => PHPExcel_Style_Border::BORDER_NONE                    
+               	)
+            ), 
+            'alignment' =>  array(
+        			'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+        			'vertical'   => PHPExcel_Style_Alignment::VERTICAL_CENTER,
+        			'rotation'   => 0,
+        			'wrap'          => TRUE
+    		)
+        );
+
+		$estiloTituloColumnas = array(
+            'font' => array(
+                'name'      => 'Arial',
+                'bold'      => true,                          
+                'color'     => array(
+                    'rgb' => 'FFFFFF'
+                )
+            ),
+            'fill' 	=> array(
+				'type'		=> PHPExcel_Style_Fill::FILL_GRADIENT_LINEAR,
+				'rotation'   => 90,
+        		'startcolor' => array(
+            		'rgb' => 'c47cf2'
+        		),
+        		'endcolor'   => array(
+            		'argb' => 'FF431a5d'
+        		)
+			),
+            'borders' => array(
+            	'top'     => array(
+                    'style' => PHPExcel_Style_Border::BORDER_MEDIUM ,
+                    'color' => array(
+                        'rgb' => '143860'
+                    )
+                ),
+                'bottom'     => array(
+                    'style' => PHPExcel_Style_Border::BORDER_MEDIUM ,
+                    'color' => array(
+                        'rgb' => '143860'
+                    )
+                )
+            ),
+			'alignment' =>  array(
+        			'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+        			'vertical'   => PHPExcel_Style_Alignment::VERTICAL_CENTER,
+        			'wrap'          => TRUE
+    		));
+			
+		$estiloInformacion = new PHPExcel_Style();
+		$estiloInformacion->applyFromArray(
+			array(
+           		'font' => array(
+               	'name'      => 'Arial',               
+               	'color'     => array(
+                   	'rgb' => '000000'
+               	)
+           	),
+           	'fill' 	=> array(
+				'type'		=> PHPExcel_Style_Fill::FILL_SOLID,
+				'color'		=> array('argb' => 'FFd9b7f4')
+			),
+           	'borders' => array(
+               	'left'     => array(
+                   	'style' => PHPExcel_Style_Border::BORDER_THIN ,
+	                'color' => array(
+    	            	'rgb' => '3a2a47'
+                   	)
+               	)             
+           	)
+        ));
+		 
+		$objPHPExcel->getActiveSheet()->getStyle('A1:F1')->applyFromArray($estiloTituloReporte);
+		$objPHPExcel->getActiveSheet()->getStyle('A3:F3')->applyFromArray($estiloTituloColumnas);		
+		$objPHPExcel->getActiveSheet()->setSharedStyle($estiloInformacion, "A4:D".($i-1));
+				
+		for($i = 'A'; $i <= 'F'; $i++){
+			$objPHPExcel->setActiveSheetIndex(0)			
+				->getColumnDimension($i)->setAutoSize(TRUE);
+		}
+		
+		// Se asigna el nombre a la hoja
+		$objPHPExcel->getActiveSheet()->setTitle('Profesores');
+
+		// Se activa la hoja para que sea la que se muestre cuando el archivo se abre
+		$objPHPExcel->setActiveSheetIndex(0);
+		// Inmovilizar paneles 
+		//$objPHPExcel->getActiveSheet(0)->freezePane('A4');
+		$objPHPExcel->getActiveSheet(0)->freezePaneByColumnAndRow(0,4);
+
+		// Se manda el archivo al navegador web, con el nombre que se indica (Excel2007)
+		header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+		header('Content-Disposition: attachment;filename="Reportedealumnos.xlsx"');
+		header('Cache-Control: max-age=0');
+
+		$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2010');
+		$objWriter->save('php://output');
+		exit;
+		
+	}
+	else{
+		print_r('No hay resultados para mostrar');
+	}
+?>
