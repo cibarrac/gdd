1b246c8c0b2135175a6d02847d6562300bf81516
C Server <c.gdd.ita@gmail.com>
2018-08-28 00:58:53 -0500

    Se agrego nombre de la secion, se comento un dropdown

--- views/menubar/navbar_boss.php
+++ views/menubar/navbar_boss.php
@@ -33,7 +33,7 @@
 
               <li class="dropdown">
                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                         Admin <span class="caret"></span></a>
+                         Jefe departamento <span class="caret"></span></a>
 
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                     <!-- <li> <a class="dropdown-item"  href="index.php?view=_Diagnostico">D N borrador</a></li> -->
