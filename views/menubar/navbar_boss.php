402367893a4a71f9c119966ccf9f6822375f79ae
C Server <c.gdd.ita@gmail.com>
2018-09-17 12:16:31 -0500

    Se comentaron unaslineas de html

--- views/menubar/navbar_boss.php
+++ views/menubar/navbar_boss.php
@@ -88,20 +88,20 @@
 
 
 
-            <li class="dropdown">
+          <!--  <li class="dropdown">
                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
                          Asistente<span class="caret"></span></a>
 
                  <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                      <li> <a class="dropdown-item"  href="index.php?view=_CargarAsistencia">   Cargar asistencias</a></li>
 
-                    <!-- <li> <a class="dropdown-item"  href="index.php?view=_Temario">Registrar temario</a></li> -->
+                     <li> <a class="dropdown-item"  href="index.php?view=_Temario">Registrar temario</a></li> -->
 
-                     <!-- <li><a class="dropdown-item"  href="index.php?view=_Grado">Registrar grado</a></li> -->
+                     <!-- <li><a class="dropdown-item"  href="index.php?view=_Grado">Registrar grado</a></li> 
 
 
                 </ul>
-               </li>
+               </li -->
 
 
 
