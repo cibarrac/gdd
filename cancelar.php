4b1d3af91b82736b2ad21a1807cbd3d741a07b5b
C Server <c.gdd.ita@gmail.com>
2018-09-17 12:20:17 -0500

    Nuva clase cancelar

---
+++ cancelar.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+include "model/common/common.php";
+include 'bs/QueryService.php';
+include_once 'bs/SQL.php'; 
+
+update();
+
+function update()
+{
+    $NumeroCurso = $_GET['curso'];
+    $firma = "sign";
+    //$public = $_GET["ispublic"];
+
+    switch (ismenu($_SESSION['username'])) {
+        case 'su': $firma.="1";break;
+        case 'admin': $firma.="2";break;
+
+    }
+
+ $SQL_UPDATE1 = "UPDATE curso SET $firma = 2 WHERE NumeroCurso = '$NumeroCurso'";
+ $SQL_UPDATE2 = "UPDATE curso SET ispublic = 2 WHERE (sign1 = 2 OR sign2 = 2) AND NumeroCurso = '$NumeroCurso'";
+ 
+ queryUpdate($SQL_UPDATE1);
+ queryUpdate($SQL_UPDATE2);
+ 
+}
+
+?>
