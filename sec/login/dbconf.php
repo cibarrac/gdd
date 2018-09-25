13a8462c9bfc55376538ab3b3037e1ec06233cde
Christian IC <cic.innsy@gmail.com>
2018-09-25 22:27:56 +0000

    Update dbconf.php

--- sec/login/dbconf.php
+++ sec/login/dbconf.php
@@ -1,9 +1,21 @@
 <?php
 //DATABASE CONNECTION VARIABLES
-$host = "localhost"; // Host name
-$username = "root"; // Mysql username
-$password = "Get7usa7"; // Mysql password
-$db_name = "gdd"; // Database name
+$host = ""; // Host name
+$username = ""; // Mysql username
+$password = ""; // Mysql password
+$db_name = ""; // Database name
+
+switch (gethostname()) {
+
+            case "C":    $host="localhost"; $username="root"; $password="root"; $db_name="gdd"; break;
+
+            case "ANALISTA-DBA":  $host="localhost"; $username="root"; $password="X@viF10r3s"; $db_name="gdd"; break;
+            
+            case "mail.innsytech.com": $host="innsytech.com"; $username="devlab_gdd"; $password="Get7usa7";  $db_name="devlab_gdd"; break;
+            
+            default:     $host="localhost";  $username="root";  $password="Get7usa7";  $db_name="gdd";
+
+        }
 
 //DO NOT CHANGE BELOW THIS LINE UNLESS YOU CHANGE THE NAMES OF THE MEMBERS AND LOGINATTEMPTS TABLES
 
