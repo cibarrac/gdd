c084ef3479daebb3610018aeaef35625a4c9adfd
Rigoberto de Jesus Nava <rigojns03@gmail.com>
2018-10-21 01:20:52 -0500

    Se agrego una consulta para bloquear las opciones del dropdown button cuando en profesor aun es autorizado por su jefe inmediato

--- model/menubar/user_table.php
+++ model/menubar/user_table.php
@@ -17,6 +17,7 @@ width: 650px;
 session_start();
 
 
+
 function getheaders($table) {
 
      $fields = querySelect(SQL::$HEADER_CURSOS);
@@ -132,19 +133,6 @@ function getheaders($table) {
     } }
     elseif($table == "profesor"){
 
-        $valida = querySelect(SQL::$VERIFICAR_ESTATUS_PROFESOR." '".$_SESSION['username']."' ");
-        foreach($valida as $col)
-        {
-            if($col['Estado'] == 0)
-            {
-                echo ' <div class= "container"> <div class="alert alert-danger" role="alert">
-                <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-                <span class="sr-only">Error:</span>
-                Aun no esta autorizado, contacte con su jefe inmediato
-                </div>  </div>';
-            }
-        }
-       
         $list = querySelect(SQL::$PERFIL_PROFESOR." '".$_SESSION['username']."' ");
          foreach($list as $row) {
         ?>
