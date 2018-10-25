9617ef4fc69be4364c595a82fa5811e394191ad6
Rigoberto de Jesus Nava <rigojns03@gmail.com>
2018-10-25 03:06:51 -0500

    verificarProfeor se corrigieron las rutas de los includes

--- bs/curso/verificaProfesor.php
+++ bs/curso/verificaProfesor.php
@@ -1,32 +1,20 @@
 <?php
 
-
-
-include "model/common/common.php";
-
-include 'bs/QueryService.php';
-
-include_once 'bs/SQL.php';
-
-
-
+include "../../model/common/common.php";
+include '../QueryService.php';
+include_once '../SQL.php'; 
 
 
 
   $correo = $_GET['correo'];
-
   $Estado = $_GET['estado'];
-
 
 
   switch ($Estado)
 
   {
-
     case 0:  $SQL = "UPDATE profesor SET Estado = 0 WHERE CorreoProfesor = '".$correo."'"; break;
-
     case 1:  $SQL = "UPDATE profesor SET Estado = 1 WHERE CorreoProfesor = '".$correo."'"; break;
-
   }
 
  queryUpdate($SQL);
