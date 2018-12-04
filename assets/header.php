6e8f07d116e4c276fce0495c3d9a60291d8d06e1
Rigo Nava <rigojns03@gmail.com>
2018-12-03 21:43:43 -0600

    Modal de los nuevos botones

--- assets/header.php
+++ assets/header.php
@@ -12,3 +12,54 @@
 <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js" charset="utf-8"></script>
 <script src="https://cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.min.js" charset="utf-8"></script>
 <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.colVis.min.js" charset="utf-8"></script>
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<!-- ideally at the bottom of the page -->
+<!-- also works in the <head> -->
+<script src="assets/alertify.js-0.3.11/js/alertify.min.js"></script>
+
+<!-- include the core styles -->
+<link rel="stylesheet" href="assets/alertify.js-0.3.11/css/alertify.core.css" />
+<!-- include a theme, can be included into the core instead of 2 separate files -->
+<link rel="stylesheet" href="assets/alertify.js-0.3.11/css/alertify.default.css" />
+
+
+<script type="text/javascript">
+ 
+    $(document).ready(function()
+    {
+        $("#simple_alert").click (function() 
+        {
+            alertify.alert("esta es mi pagina de alerta");
+        });
+
+
+
+        $("#simple_confirm").click (function() 
+        {
+            alertify.confirm("Este es el titulo de confirmacion", function(e) {
+                if(e){alertify.alert("Has aceptado las condiciones!");}
+                else{alertify.alert("No has aceptado las condiciones!");}
+            });
+        });
+
+        $("#simple_prompt").click (function() 
+        {
+            alertify.prompt("Por favor, ingresa nuevamente tu contraseña", function(e,srt) {
+                if(e){alertify.alert(srt);}
+                else{alertify.alert("Error de logueo");}
+            });
+        });
+
+
+
+
+
+    });
+
+    
+
+
+</script>
