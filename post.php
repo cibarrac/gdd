4548a699a768c06e7562207e7e2967d75fb2cfe0
Rigo Nava <rigojns03@gmail.com>
2018-12-04 00:55:52 -0600

    Se indento y se corrigio post.php (las validaciones ya funcionan)

--- post.php
+++ post.php
@@ -12,277 +12,213 @@ include 'login/register.php';
 
  */
 
-function dialog($msg){
+function dialog($msg)
+{
     $view = $_POST['view'];
     echo '<script> alert("'.$msg.'");
         window.location.href="index.php";
          </script> ';
 }
 
-function isValidProfesor()
+function isValidInscripcion($IdCurso, $IdProfesor) 
 {
-    $email = $formData['CorreoProfesor'];
-    $profesorRegistrado = CursoService::getProfesorRegistrado($email);
-    if($profesorRegistrado == false)
-    {
-        dialog('Usted ya se encuetra registrado');
-        return false;
-    }
-    else { 
-        return true; 
-    }
-}
-function isValidCurso() {
-
-    $idAula = $formData['AulaPropuesta'];
-    $turno = $formData['Turno'];
-    $aula = CursoService::getAulaDisponible($idAula, $turno);
-    if($aula == false)
-    {
-       dialog('El aula propuesta esta ocupada, seleccione otra');
-       return false;
-    } 
-    else { 
-        return true; 
-    }
-}
-
-function isValidInscripcion() {
-    $IdCurso = $formData['NumeroCurso'];
-
-    $IdProfesor = $formData['IdProfesor'];
     $cursoAprobado = CursoService::getCursoAprobado($IdCurso);
     $profesor = CursoService::getProfesorInscrito($IdProfesor, $IdCurso); 
     $fechalimite = CursoService::getFechaLimiteInscripcion($IdCurso);
     $cupo = CursoService::getLimiteCurso($IdCurso);
     $turno = CursoService::getTurnoCurso($IdCurso, $IdProfesor);
-
-    if($cursoAprobado == false) {
-
-        dialog('No puede hacer una inscripcion, el curso no esta apobado');
-
-        return false;
-
-    }
-
-    if($profesor == false)
-
-    {
-
-        dialog('No se puede inscribir, ya esta inscrito');
-
-        return false;
-
-    } 
-
-    elseif($fechalimite == false)
-
-    {
-
-        dialog('No puede inscribirse al curso, la fecha limite expiro ');
-
-        return false;   
-
-    }
-
-    elseif($cupo == false)
-
-    {
-
-        dialog('No puede inscribirse al curso, el curso esta lleno');
-
-        return false;
-
-    }
-
-    elseif($turno == false)
-
-    {
-
-        dialog('No puede inscrirse al curso, ya tiene un curso en el mismo turno');
-
-        return false;
-
-    }
-
     
 
-    else { return true; }
-   
+    if($cursoAprobado == false) 
+    {
+        dialog('No puede hacer una inscripcion, el curso no esta aprobado');
+        return false;
+    }
+    elseif($profesor == false)
+    {
+        dialog('No se puede inscribir, ya esta inscrito');
+        return false;
+    } 
+    elseif($fechalimite == false)
+    {
+        dialog('No puede inscribirse al curso, la fecha limite expiro ');
+        return false;   
+    }
+    elseif($cupo == false)
+    {
+        dialog('No puede inscribirse al curso, el curso esta lleno');
+        return false;
+    }
+    elseif($turno == false)
+    {
+        dialog('No puede inscrirse al curso, ya tiene un curso en el mismo turno');
+        return false;
+    }
+    else 
+    {
+        queryInsert("INSERT INTO asistencias (IdProfesorA, NumeroCursoA) VALUES ('".$IdProfesor."' , '".$IdCurso."') ");
+        return true; 
+    }
 }
-function isValidJefe()
-{
-    $idDepartamento = $formData['IdDepartamentoJefe'];     
 
+function isValidCurso($idAula, $turno) 
+{
+    $aula = CursoService::getAulaDisponible($idAula, $turno);
+
+    if($aula == false)
+    {
+       dialog('El aula propuesta esta ocupada, seleccione otra');
+       return false;
+    } 
+    else 
+    { 
+        return true; 
+    }
+}
+
+function isValidProfesor($email)
+{
+    $profesorRegistrado = CursoService::getProfesorRegistrado($email);
+
+    if($profesorRegistrado == false)
+    {
+        dialog('Usted ya se encuetra registrado');
+        return false;
+    }
+    else 
+    { 
+        return true; 
+    }
+}
+
+function isValidJefe($idDepartamento)
+{   
     $jefeRegistrado = CursoService::getJefeDepartamentoRegistrado($idDepartamento);
 
-    
-
     if($jefeRegistrado == false)
-
     {
-
         dialog('El departamento ya cuenta con un jefe registrado');
-
         return false;
-
     }
-
-    else { return true; }
+    else 
+    { 
+        return true; 
+    }
 }
-function isValidDepartamento()
-{
-    $nombreDepto = $formData['NombreDepartamento'];
 
+function isValidDepartamento($nombreDepto)
+{
     $departamentoRegistrado = CursoService::getDepartamentoRegistrado($nombreDepto);
 
-    
-
     if($departamentoRegistrado == false)
-
     {
-
         dialog('El departamento ya esta registrado');
-
         return false;
-
     }
-
-    else { return true; }
+    else 
+    {
+        return true; 
+    }
 }
 
-function isValidCarrera()
+function isValidCarrera($nombreCarrera)
 {
-    $nombreCarrera = $formData['NombreCarrera'];
-
     $carreraRegistrada = CursoService::getCarreraRegistrada($nombreCarrera);
 
-    
-
     if($carreraRegistrada == false)
-
     {
-
        dialog('La carrera ya esta registrada');
-
        return false; 
-
     }
-
-    else { return true; }
+    else 
+    { 
+        return true; 
+    }
 }
+
 function isValidEscuela()
 {
     $infoEscuela = CursoService::getInfoEscuela();
 
-        
-
     if($infoEscuela == false)
-
     {
-
         dialog('La inforamcion de la institucion ya esta en el sistema');
-
         return false;
-
     }
-
-    else { return true; }
+    else
+    { 
+        return true; 
+    }
 }
-function validation(){
 
+
+function validation()
+{
     $formData = getPOST_GET();
-
     $table = $formData['table'];
-     
-    switch ($table) {
-        
-        case '"inscripcion"': return isValidInscripcion(); break;
-        
-        default:
-            # code...
-            break;
-    }
 
-    if($table=="curso")
+    $IdCurso = $formData['NumeroCurso'];
+    $IdProfesor = $formData['IdProfesor'];
+
+    $idAula = $formData['AulaPropuesta'];
+    $turno = $formData['Turno'];
+
+    $email = $formData['CorreoProfesor'];
+
+    $idDepartamento = $formData['IdDepartamentoJefe'];  
+
+    $nombreDepto = $formData['NombreDepartamento'];
+
+    $nombreCarrera = $formData['NombreCarrera'];
+
+    switch($table)
     {
-           return isValidCurso();
+        case "inscripcion": return isValidInscripcion($IdCurso, $IdProfesor); break;
+        case "curso": return isValidCurso($idAula, $turno); break;
+        case "profesor": return isValidProfesor($email); break;
+        case "jefedepartamento": return isValidJefe($idDepartamento); break;
+        case "departamento": return isValidDepartamento($nombreDepto); break;
+        case "carrera": return isValidCarrera($nombreCarrera); break;
+        case "infoescuela": return isValidEscuela(); break;
+        default; true; break;
     }
-    if($table == "profesor" )
-    {
-          return isValidProfesor();
-    }
-    if($table == "jefedepartamento")
-    {
-          return isValidJefe();
-    }
-
-    if($table == "departamento")
-
-    {
-
-       return isValidDepartamento();
-
-    }
-    if($table == "carrera")
-     {
-       return isValidCarrera();
-     }
-
-    if($table == "infoescuela")
-    {
-
-      return isValidEscuela();
-
-    }
-  
-
-    else { return true; }
-
 }
 
-function save($table) {
+function save($table) 
+{
+    $config = ["fields"=>"(","values"=>"(","update_v"=>" "];
+    $i = 0; $id_use = 3;
+    $_HTTP = getPOST_GET();
 
-             $config = ["fields"=>"(","values"=>"(","update_v"=>" "];
+    foreach($_HTTP as $KEY => $VALUE)
+    {
+        if($i<(count($_HTTP )) && $i > $id_use) 
+        {
 
-             $i = 0; $id_use = 3;
+            if ($i<(count($_HTTP)-1)) 
+            {  
+                $coma = ",";
+            }
+            else 
+            {
+                $coma = "";
+            }
 
-              $_HTTP = getPOST_GET();
+            $config['fields'].=$KEY.$coma;
+            $config['values'].="'".$VALUE."'".$coma;
+        }
 
-                  foreach($_HTTP as $KEY => $VALUE){
-
-                       if($i<(count($_HTTP )) && $i > $id_use) {
-
-                                if ($i<(count($_HTTP)-1)) {  $coma = ",";}
-
-                                   else {
-
-                                        $coma = "";
-
-                                   }
-
-                                 $config['fields'].=$KEY.$coma;
-
-                                 $config['values'].="'".$VALUE."'".$coma;
-
-                               }
-
-                               $i++;
-
-                  }
-
-                  if(validation()) {
-
-                  $SQL_INSERT = "INSERT INTO ". $table." ".$config['fields'].") VALUES ".$config['values'].")";
-
-                  queryInsert($SQL_INSERT);
-
-                  }
-
-                  else{dialog('No se guardó'); }
-
-                 
-
+        $i++;
+    }
+    if(validation()) 
+    {
+        $SQL_INSERT = "INSERT INTO ". $table." ".$config['fields'].") VALUES ".$config['values'].")";
+        queryInsert($SQL_INSERT);
+    }
+    else
+    {
+        dialog('No se guardó'); 
+    }
 }
 
 /**
@@ -295,38 +231,32 @@ function save($table) {
 
  */
 
-function update($id,$table) {
+function update($id,$table) 
+{
+    $config = ["fields"=>"(","values"=>"","update_v"=>" "];
+    $values =  $config['values'];
+    $_HTTP = getPOST_GET();   $i=0; $id_use = 3;
 
-     $config = ["fields"=>"(","values"=>"","update_v"=>" "];
+    foreach($_HTTP as $KEY => $VALUE)
+    {
+        if($i<(count($_HTTP)) && $i > $id_use) 
+        {
+            if ($i<(count($_HTTP)-1)) 
+            {  
+                $coma = ",";
+            }
 
-     $values =  $config['values'];
-
-     $_HTTP = getPOST_GET();   $i=0; $id_use = 3;
-
-     foreach($_HTTP as $KEY => $VALUE){
-
-          if($i<(count($_HTTP)) && $i > $id_use) {
-
-                  if ($i<(count($_HTTP)-1)) {  $coma = ",";}
-
-                  else { $coma = ""; }
-
-                    //$config['fields'].=$KEY.$coma;
-
-                    $values.="$KEY='".$VALUE."' ".$coma;
-
-                  }
-
-                  $i++;
-
-      }
-
-                    $SQL_UPDATE = "UPDATE ". $table." SET ".$values." WHERE IdProfesor = '".$id."'";
-
-                    echo $SQL_UPDATE;
-
-                    queryUpdate($SQL_UPDATE);
-
+            else 
+            { 
+                $coma = ""; 
+            }
+            $values.="$KEY='".$VALUE."' ".$coma;
+        }
+        $i++;
+    }
+    $SQL_UPDATE = "UPDATE ". $table." SET ".$values." WHERE IdProfesor = '".$id."'";
+    echo $SQL_UPDATE;
+    queryUpdate($SQL_UPDATE);
 }
 
 /**
@@ -343,54 +273,45 @@ function update($id,$table) {
 
  */
 
-function show($list) {
-
-  foreach($list as $KEY => $VALUE){
-
-             echo $KEY;
-
-}
-
-}
-
-function getPOST_GET() {
-
-    if(!empty($_POST)){
-         echo " <br>";
-         return $_POST;
+function show($list) 
+{
+    foreach($list as $KEY => $VALUE)
+    {
+        echo $KEY;
     }
-    else if(!empty($_GET)) {
-      echo " <br>";
-      return $_GET;
+}
 
+function getPOST_GET() 
+{
+    if(!empty($_POST))
+    {
+        echo " <br>";
+        return $_POST;
+    }
+
+    elseif(!empty($_GET)) 
+    {
+        echo " <br>";
+        return $_GET;
     } 
-
 }
 
-function isvalid() {
-
-               foreach($_POST as $KEY => $VALUE){
-
-                            if($VALUE=='') {
-
-                             return false;
-
-                             echo "Falta un campo";
-
-                            }
-
-               }
-
-               return true;
-
+function isvalid() 
+{
+    foreach($_POST as $KEY => $VALUE)
+    {
+        if($VALUE=='') 
+        {
+            return false;
+            echo "Falta un campo";
+        }
+    }
+    return true;
 }
 
-switch ($_POST['action']) {
-
-          case 'save':  save($_POST['table']); break;
-
-          case 'update':  update($_POST['IdProfesor'],$_POST['table']); break;
-
-          default: echo "Falta el atributo action en el formulario"; break;
-
+switch ($_POST['action']) 
+{
+    case 'save':  save($_POST['table']); break;
+    case 'update':  update($_POST['IdProfesor'],$_POST['table']); break;
+    default: echo "Falta el atributo action en el formulario"; break;
 }
